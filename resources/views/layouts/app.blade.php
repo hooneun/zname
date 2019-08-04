43c210d973fc8b8271ff10eb7959adb8b510e6ac
khon0316 <kimhoon0316@gmail.com>
2019-08-04 20:49:22 +0900

    favicon or logo 추가

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -24,7 +25,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'ZCARD') }}
+                <img src="{{ asset('images/logo.png') }}" style="height: 16px;">
             </a>
             @auth
                 <a href="{{ route('logout') }}" class="btn btn-light"

1c3f98b189aa1f33116985e3ba0a3379c3c3618f
khon0316 <kimhoon0316@gmail.com>
2019-09-07 20:26:47 +0900

    -

--- resources/views/cards/register.blade.php
+++ resources/views/cards/register.blade.php
@@ -166,7 +166,7 @@
                             <div id="video_wrapper" class="{{ !empty($card->youtube) ? 'hide' : '' }}">
                                 <img id="lets_change_video" src="{{ asset('images/card/myyoutube.png') }}" onclick="changeTag()";>
                             </div>
-                            <div id="add_video_wrapper" class="{{ !empty($card->youtube) ? 'show zmt-10' : '' }}">
+                            <div id="add_video_wrapper" class="{{ !empty($card->youtube) ? 'show' : '' }} zmt-10">
                                 <textarea id="add_youtube" name="youtube" tyle="text" placeholder="유튜브 공유하기 링크를 넣어주세요."></textarea>
                                 <button id="complete_addyoutube" type="button" onclick="completeAddvideo()">완료</button>
                             </div>
